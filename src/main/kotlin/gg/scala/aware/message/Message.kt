a4c09527e5ba1253826f2606702ec9db893460e7
GrowlyX <growlyxdev@gmail.com>
2023-07-30 00:54:25 -0400

    chore: use the configured json instance and add option to pass in a custom serialization strategy

--- src/main/kotlin/gg/scala/aware/message/Message.kt
+++ src/main/kotlin/gg/scala/aware/message/Message.kt
@@ -2,8 +2,11 @@ package gg.scala.aware.message
 
 import gg.scala.aware.Aware
 import gg.scala.aware.AwareHub
+import gg.scala.aware.AwareHub.jsonInstance
 import gg.scala.aware.thread.AwareThreadContext
+import kotlinx.serialization.ContextualSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationStrategy
 import kotlinx.serialization.Transient
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
@@ -23,16 +26,26 @@ class Message(
 {
     val content = mutableMapOf<String, JsonElement>()
 
-    inline fun <reified T> get(id: String) = Json
+    inline fun <reified T> get(id: String) = jsonInstance
         .decodeFromJsonElement<T>(
             checkNotNull(content[id]) {
                 "Message content by id $id is not available"
             }
         )
 
+    inline fun <reified T : Any> set(
+        key: String, value: T,
+        serializer: SerializationStrategy<T>
+    ) = apply {
+        content[key] = jsonInstance
+            .encodeToJsonElement(
+                serializer, value
+            )
+    }
+
     inline operator fun <reified T> set(key: String, value: T) =
         apply {
-            content[key] = Json.encodeToJsonElement(value)
+            content[key] = jsonInstance.encodeToJsonElement(value)
         }
 
     suspend fun publish(
